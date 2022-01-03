f58d2290812ffc229b82e4b41adc3600b7bc0b81
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-01-03 12:39:35 +0000

    Initial Commit: Basic structure and main class.

---
+++ src/com/librarymanagementsystem/Main.java
@@ -0,0 +1,12 @@
+package com.librarymanagementsystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("*** Prince A.K. Adewunmi Memorial University ***" +
+                "\n*** A Leading STEM Institution ***" +
+                "\n*** Central Library System ***" +
+                "\n*** Welcome! ***");
+    }
+}
