0d8be1f0d2387c28e8794e528872f62bf6b98772
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-01-07 16:57:42 +0000

    6h Commit: Created Transactions class.

---
+++ src/com/librarymanagementsystem/transactions/Transactions.java
@@ -0,0 +1,9 @@
+package com.librarymanagementsystem.transactions;
+
+/**
+ * @author Adrian Adewunmi
+ * @date: 07/01/2022
+ * This class is used to define library transactions e.g. borrowing, returning.
+ */
+public class Transactions {
+}
