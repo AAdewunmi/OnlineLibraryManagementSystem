0a7f3d823e93f34a9fe10a38b045f6290a1cc1ee
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-01-08 15:53:39 +0000

    8h Commit: Created BookCatalogue class.

--- src/com/librarymanagementsystem/books/Book.java
+++ src/com/librarymanagementsystem/books/Book.java
@@ -1,39 +1,37 @@
 package com.librarymanagementsystem.books;
 
+import com.sun.org.apache.xpath.internal.functions.FuncSubstring;
+
 /**
  * @author Adrian Adewunmi
  * @date 06/01/2022
  * This class is used to create a book object
  */
 public class Book {
-    private final String bookId;
-    private final String bookName;
-    private final String author;
+    private final String ISBN;
+    private final String bookTitle;
     private final String publisher;
     private final String edition;
-    private final String category;
-    private int quantity;
+    private final String subject;
+    private final int numberOfPages;
+    private final String publicationDate;
 
-    public Book(String bookId, String bookName, String author, String publisher, String edition, String category, int quantity) {
-        this.bookId = bookId;
-        this.bookName = bookName;
-        this.author = author;
+    public Book(String ISBN, String bookTitle, String publisher, String edition, String subject, int numberOfPages, String publicationDate) {
+        this.ISBN = ISBN;
+        this.bookTitle = bookTitle;
         this.publisher = publisher;
         this.edition = edition;
-        this.category = category;
-        this.quantity = 0;
+        this.subject = subject;
+        this.numberOfPages = 0;
+        this.publicationDate = publicationDate;
     }
 
-    public String getBookId() {
-        return bookId;
+    public String getISBN() {
+        return ISBN;
     }
 
-    public String getBookName() {
-        return bookName;
-    }
-
-    public String getAuthor() {
-        return author;
+    public String getBookTitle() {
+        return bookTitle;
     }
 
     public String getPublisher() {
@@ -44,15 +42,25 @@ public class Book {
         return edition;
     }
 
-    public String getCategory() {
-        return category;
+    public String getSubject() {
+        return subject;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getNumberOfPages() {
+        return numberOfPages;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public String toString(){
+        return "\nISBN: " + this.ISBN + "\n"
+                + "Book Title: " + this.bookTitle + "\n"
+                + "Publisher: " + this.publisher + "\n"
+                + "Edition: " + this.edition + "\n"
+                + "Subject: " + this.subject + "\n"
+                + "Number of Pages: " + this.numberOfPages + "\n"
+                + "Publication Date: " + this.publicationDate + "\n";
     }
 }
---
+++ src/com/librarymanagementsystem/books/BookCatalogue.java
@@ -0,0 +1,42 @@
+package com.librarymanagementsystem.books;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Adrian Adewunmi
+ * @date 08/01/2022
+ * This class is used to create a book catalogue object
+ */
+public class BookCatalogue {
+    private String creationDate;
+    private int totalBookAuthors;
+    private ArrayList<BookAuthor> sciences;
+    private HashMap<String, ArrayList<BookAuthor>> science;
+    private HashMap<String, ArrayList<BookAuthor>> technology;
+    private HashMap<String, ArrayList<BookAuthor>> engineering;
+    private HashMap<String, ArrayList<BookAuthor>> mathematics;
+    // TODO: 08/01/2022: Update the book catalogue.
+    // 1. Insert a new book into a category
+    // 2. Search for a book in a category
+    // 3. Remove a book from a category
+    // 4. Sorting!!!
+    // Test Class: BookCatalogueTest.java
+    /*public static void main(String[] args) {
+        // books
+        ArrayList<BookAuthor> scienceBookAuthors = new ArrayList<>();
+        scienceBookAuthors.add(new BookAuthor("123", "Physics Today", "Elsevier", "1st",
+                "Science", 1000, "Joe Bull", "22-01-2020"));
+        scienceBookAuthors.add(new BookAuthor("234", "Chemistry Tomorrow", "Elsevier", "2st",
+                "Science", 1234,"James Last", "02-05-1999"));
+        scienceBookAuthors.add(new BookAuthor("456", "Biology Forever", "Elsevier", "4st",
+                "Science", 600, "Bill First", "11-07-1921"));
+        // catalogue
+        HashMap<String, ArrayList<BookAuthor>> science = new HashMap<>();
+        science.put("Science Category ", scienceBookAuthors);
+        science.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + ":\n" + entry.getValue() + "\n");
+        } );
+    }*/
+}
