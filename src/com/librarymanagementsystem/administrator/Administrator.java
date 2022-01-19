649b1785aa2e849e9e452bcd6ec39fdf12dfbdd7
Adrian Adewunmi <adrian.a.adewunmi@googlemail.com>
2022-01-19 14:42:58 +0000

    Delete Administrator.java

--- src/com/librarymanagementsystem/administrator/Administrator.java
+++
@@ -1,9 +0,0 @@
-package com.librarymanagementsystem.administrator;
-
-/**
- * @author Adrian Adewunmi
- * @date 06/01/2022
- * This class is used to create an Administrator object
- */
-public class Administrator {
-}
