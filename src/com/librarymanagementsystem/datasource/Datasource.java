9e624ac3e21d85b87e053623a06b144094b7945a
Adrian Adewunmi <adrian.a.adewunmi@googlemail.com>
2022-01-19 14:43:30 +0000

    Delete Datasource.java

--- src/com/librarymanagementsystem/datasource/Datasource.java
+++
@@ -1,9 +0,0 @@
-package com.librarymanagementsystem.datasource;
-
-/**
- * @author Adrian Adewunmi
- * @date 06/01/2022
- * This class is used to connect to the database
- */
-public class Datasource {
-}
